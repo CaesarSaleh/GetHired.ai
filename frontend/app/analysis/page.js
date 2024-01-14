5200b7b7443b823116e0bb70eb0c4442ff07ac86
MagneticGlitter <92243872+MagneticGlitter@users.noreply.github.com>
2024-01-14 02:11:51 -0500

    j

--- frontend/app/analysis/page.js
+++ frontend/app/analysis/page.js
@@ -1,7 +1,6 @@
 "use client";
 import { Radar } from 'react-chartjs-2';
 import Chart from 'chart.js/auto';
-
 import { Menu, Popover } from "@headlessui/react";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
