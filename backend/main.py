af61bf431cb9981eb0b6dd4eee750aaf9aaef91a
CaesarSaleh <caesarssaleh@gmail.com>
2024-01-14 05:32:59 -0500

    added sql

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,37 @@
 from flask import Flask, jsonify, request
+import sqlite3
 
 
 app = Flask(__name__)
+
+
+def init_db():
+    conn = sqlite3.connect('tokens.db')
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS tokens (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            tokenid TEXT NOT NULL
+        )
+    ''')
+
+    conn.commit()
+    conn.close()
+
+
+def empty_tokens_table():
+    conn = sqlite3.connect('tokens.db')
+    cursor = conn.cursor()
+
+    # Delete all rows from the tokens table
+    cursor.execute('DELETE FROM tokens')
+
+    conn.commit()
+    conn.close()
+
+
+init_db()
 
 
 def add_cors_headers(response):
@@ -14,6 +44,50 @@ def add_cors_headers(response):
 @app.route('/')
 def index():
     return 'hello'
+
+
+@app.route('/add_to_sql', methods=['POST'])
+def add_to_sql():
+    new_token = request.json
+    tokenId = new_token.get('jobDesc')
+
+    if not tokenId:
+        return jsonify({'error': 'Token ID is required'}), 400
+
+    conn = sqlite3.connect('tokens.db')
+    cursor = conn.cursor()
+
+    try:
+        # Insert the new token into the database
+        cursor.execute("INSERT INTO tokens (tokenId) VALUES (?)", (tokenId,))
+        conn.commit()
+
+        # Retrieve the inserted token ID
+        token_id = cursor.lastrowid
+
+        return jsonify({'success': True, 'id': token_id, 'tokenId': tokenId})
+
+    except sqlite3.IntegrityError:
+        return jsonify({'error': 'Token ID must be unique'}), 400
+
+    finally:
+        conn.close()
+
+
+@app.route('/get_from_sql', methods=['GET'])
+def get_from_sql():
+    # Use a context manager to ensure proper resource management
+    with sqlite3.connect('tokens.db') as conn:
+        cursor = conn.cursor()
+
+        # Retrieve the latest token based on timestamp or ID
+        cursor.execute('SELECT * FROM tokens LIMIT 1')
+        token = cursor.fetchone()
+
+    if token:
+        return jsonify({'tokenId': token[1]})
+    else:
+        return jsonify({'message': 'No tokens available'})
 
 
 @app.route('/run_cohere_analysis', methods=['POST'])
@@ -34,11 +108,11 @@ def run_cohere_analysis():
     feedback = tool.get_response_feedback()
 
     # Returning data
-    return_data = {"sentiment": sentiment, "confidence": confidence, "feedback": feedback}
+    return_data = {"sentiment": sentiment,
+                   "confidence": confidence, "feedback": feedback}
     return jsonify(return_data)
 
 
 if __name__ == '__main__':
     app.after_request(add_cors_headers)
     app.run(port=4000, debug=True)
-
